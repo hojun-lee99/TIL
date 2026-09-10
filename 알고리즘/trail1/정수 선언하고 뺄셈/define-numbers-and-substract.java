ba96499947ff9db0914d42cabe546b931a0403c9
이호준 <hijun990810@gmail.com>
2026-09-10 19:59:10 +0900

    [Passed] 정수 선언하고 뺄셈 (94ms, 9MB)

---
+++ 알고리즘/trail1/정수 선언하고 뺄셈/define-numbers-and-substract.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int n1 = 97;
+        int n2 = 13;
+        int sub = n1 - n2;
+
+        System.out.print(n1 + " - " + n2 + " = " + sub);
+    }
+}
