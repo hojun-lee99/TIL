6e3fd935a9ba41be001b29e800989fe8f40713cb
이호준 <hijun990810@gmail.com>
2026-09-10 20:22:19 +0900

    [Passed] 입력받아 계산 (134ms, 10MB)

---
+++ 알고리즘/trail1/입력받아 계산/input-calculate.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner stdin = new Scanner(System.in);
+
+        int a = stdin.nextInt();
+
+        int sum = a + 2;
+
+        System.out.print(sum);
+    }
+}
