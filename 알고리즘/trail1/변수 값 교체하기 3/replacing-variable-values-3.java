5a3f761b25e3828c34e948044ebc98a6f2cc61ee
이호준 <hijun990810@gmail.com>
2026-09-10 20:13:33 +0900

    [Passed] 변수 값 교체하기 3 (89ms, 9MB)

---
+++ 알고리즘/trail1/변수 값 교체하기 3/replacing-variable-values-3.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int a = 3;
+        int b = 5;
+        int temp;
+
+        temp = b;
+        b = a;
+        a = temp;
+
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
