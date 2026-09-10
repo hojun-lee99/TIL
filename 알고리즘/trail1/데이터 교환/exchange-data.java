322baada41a5d7026d0a0a3bd1a6a01ace88621b
이호준 <hijun990810@gmail.com>
2026-09-10 20:16:11 +0900

    [Passed] 데이터 교환 (93ms, 9MB)

---
+++ 알고리즘/trail1/데이터 교환/exchange-data.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int a = 5;
+        int b = 6;
+        int c = 7;
+        int temp;
+
+        temp = a;
+        a = b;
+        b = temp;
+
+        temp = a;
+        a = c;
+        c = temp;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
