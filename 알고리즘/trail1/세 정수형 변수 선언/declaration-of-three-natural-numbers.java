1b3e885a650a888ecc515294095154f096c88d57
이호준 <hijun990810@gmail.com>
2026-09-10 20:05:23 +0900

    [Passed] 세 정수형 변수 선언 (93ms, 9MB)

---
+++ 알고리즘/trail1/세 정수형 변수 선언/declaration-of-three-natural-numbers.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int a = 7;
+        int b = 23;
+        int sum = a + b;
+
+        System.out.print(a + " + " + b + " = " + sum);
+    }
+}
