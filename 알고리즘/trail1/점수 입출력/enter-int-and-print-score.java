086b2e8ba54719e0890e853d04e08ad63a47a364
이호준 <hijun990810@gmail.com>
2026-09-11 12:42:13 +0900

    [Passed] 점수 입출력 (132ms, 10MB)

---
+++ 알고리즘/trail1/점수 입출력/enter-int-and-print-score.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner stdin = new Scanner(System.in);
+
+        int n = stdin.nextInt();
+
+        System.out.println("Your score is " + n + " point.");
+    }
+}
