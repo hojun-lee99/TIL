e514942acc1ff2dea39fbdf0b5892bbb76c30609
이호준 <hijun990810@gmail.com>
2026-09-11 12:47:05 +0900

    [Passed] 정수 입력받아 계산 (138ms, 10MB)

---
+++ 알고리즘/trail1/정수 입력받아 계산/calculation-by-inputting-an-integer.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner stdin = new Scanner(System.in);
+
+        int a = stdin.nextInt();
+
+        System.out.print(a * 2 + 3);
+    }
+}
