36fe5bfef736efe1fc9a8402c51b67350cad5eee
이호준 <hijun990810@gmail.com>
2026-09-19 14:11:35 +0900

    [Passed] 수의 계절은 (133ms, 10MB)

---
+++ 알고리즘/trail1/수의 계절은/season-of-num.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        int m = sc.nextInt();
+
+        if (m == 12 || m <= 2) {
+            System.out.print("Winter");
+        }
+        else if (m <= 5) {
+            System.out.print("Spring");
+        }
+        else if (m <= 8) {
+            System.out.print("Summer");
+        }
+        else {
+            System.out.print("Fall");
+        }
+    }
+}
