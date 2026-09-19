24e4894e082c052e619aa61305ed779828d0f6b9
이호준 <hijun990810@gmail.com>
2026-09-19 13:18:36 +0900

    [Passed] 점수 비교 (127ms, 10MB)

---
+++ 알고리즘/trail1/점수 비교/score-comparison.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        int aMath = sc.nextInt();
+        int aEnglish = sc.nextInt();
+        int bMath = sc.nextInt();
+        int bEnglish = sc.nextInt();
+
+        if (aMath > bMath && aEnglish > bEnglish) {
+            System.out.println(1);
+        }
+        else {
+            System.out.println(0);
+        }
+    }
+}
